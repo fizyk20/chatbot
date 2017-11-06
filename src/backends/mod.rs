fa3fe7fada07c44d259656ab81ca8e7e3f13ebe4
Bartłomiej Kamiński <fizyk20@gmail.com>
2017-11-07 01:57:59 +0800

    Modify BotBackend to (hopefully) be object-safe

--- src/backends/mod.rs
+++ src/backends/mod.rs
@@ -1,5 +1,5 @@
 use core::{BackendChannel, BackendEvent, MessageContent};
-use std::convert::Into;
+use serde_json::Value;
 
 #[derive(Clone, Copy, Debug, Serialize, Deserialize)]
 pub enum BackendType {
@@ -8,15 +8,20 @@ pub enum BackendType {
     Discord,
 }
 
-pub trait BotBackend {
-    type LoginData;
-    type ChannelName;
-    type Error;
+quick_error! {
+    #[derive(Debug)]
+    pub enum BackendError {
+        LoginDataInvalid {}
+    }
+}
 
+pub type BackendResult<T> = Result<T, BackendError>;
+
+pub trait BotBackend {
     fn get_type(&self) -> BackendType;
-    fn connect(&mut self, login_data: Self::LoginData) -> Result<(), Self::Error>;
-    fn join<T: Into<Self::ChannelName>>(&mut self, channel: T) -> Result<(), Self::Error>;
-    fn send(&mut self, dst: BackendChannel, msg: MessageContent) -> Result<(), Self::Error>;
-    fn reconnect(&mut self) -> Result<(), Self::Error>;
-    fn get_event(&mut self) -> Result<Option<BackendEvent>, Self::Error>;
+    fn connect(&mut self, login_data: Value) -> BackendResult<()>;
+    fn join(&mut self, channel: &str) -> BackendResult<()>;
+    fn send(&mut self, dst: BackendChannel, msg: MessageContent) -> BackendResult<()>;
+    fn reconnect(&mut self) -> BackendResult<()>;
+    fn get_event(&mut self) -> BackendResult<Option<BackendEvent>>;
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 extern crate chrono;
 #[macro_use]
 extern crate lazy_static;
+#[macro_use]
+extern crate quick_error;
 extern crate serde_json;
 #[macro_use]
 extern crate serde_derive;
